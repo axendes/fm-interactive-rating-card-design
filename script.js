49f8574a1260ab95d6596feb15166d37d2e2ff25
Alex <64488414+hiozen@users.noreply.github.com>
2023-04-27 20:18:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 "use strict";
 
-const submitBtn = document.querySelector(".face__submit");
-const resultsPar = document.querySelector(".back__user-selected");
-const faceCont = document.querySelector(".wrapper__inner_face");
-const backCont = document.querySelector(".wrapper__inner_back");
+const sbmt = document.querySelector(".wrapper-face__form-submit");
+const resultTo = document.querySelector(".wrapper-back__selected");
+const faceCard = document.querySelector(".wrapper-face");
+const backCard = document.querySelector(".wrapper-back");
+const inputs = document.querySelectorAll(".wrapper-face__form-input");
 
-submitBtn.onclick = showResults;
+inputs.forEach((e) => {
+    e.addEventListener("change", () => {
+        sbmt.removeAttribute("disabled");
+    }, { once: true });
+})
 
-function showResults() {
-    const checkedRadio = document.querySelector("input[name=face__radio]:checked");
-    if (!!checkedRadio) {
-        resultsPar.innerHTML = `You selected ${checkedRadio.id.slice(-1)} out of 5`;
-        faceCont.style.display = "none";
-        backCont.style.display = "flex";
+sbmt.onclick = showBackCard;
+
+function showBackCard() {
+    let rating = faceCard.querySelector(".wrapper-face__form-input:checked").previousSibling.nodeValue.trim();
+
+    resultTo.textContent = resultTo.textContent.replace("*", rating);
+    
+    faceCard.onanimationend = () => {
+        faceCard.style.display = "none";
+        backCard.style.display = "flex";
+        backCard.classList.add("wrapper-back--activated");
     }
+
+    faceCard.classList.add("wrapper-face--deactivated");
 }
